cadcc73098ed8e0b5b020153c2d581d9095dbb10
idk <problemsolver@openmailbox.org>
2018-01-29 04:17:31 -0500

    I wonder if this will work.

--- rk-tools/Android.mk
+++ rk-tools/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS := -g -O3 -DUSE_OPENSSL -Wall
+LOCAL_C_INCLUDES := external/openssl/include $(LOCAL_C_INCLUDES)
 LOCAL_LDLIBS := -lssl -lcrypto
 LOCAL_SHARED_LIBRARIES += libssl libcrypto
 LOCAL_SRC_FILES := afptool.c
