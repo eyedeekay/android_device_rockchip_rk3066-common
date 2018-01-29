5cade491a683a83546a42bc2be6acd464b0f2c2c
idk <problemsolver@openmailbox.org>
2018-01-29 01:41:55 -0500

    build tools with mk808b instead of regular mk808.

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 LOCAL_OVERRIDES_PACKAGES := CMUpdater
 
-ifneq ($(filter mk808,$(TARGET_DEVICE)),)
+ifneq ($(filter mk808b,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
